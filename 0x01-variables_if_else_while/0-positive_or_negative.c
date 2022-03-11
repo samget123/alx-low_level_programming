684fc2cd528e31270f091d265ea68b8e029739fb
samget123 <67164756+samget123@users.noreply.github.com>
2022-03-11 23:12:51 +0300

    positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,15 +11,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("is positive")
+		printf("is positive");
 	}
 	if (n < 0)
 	{
-		printf("is negative")
+		printf("is negative");
 	}
 	if (n == 0)
 	{
-		printf("is zero")
+		printf("is zero");
 	}
 	return (0);
 }
